ddad6f91e3cdd049824ebf21e3a7033682820681
Nahomie ND <nducasse3@gmail.com>
2020-12-07 19:22:02 +0000

    Automatically backed up by Learn

--- add-elements.js
+++ add-elements.js
@@ -2,3 +2,6 @@ var elementOne = document.createElement('div');
 elementOne.innerHTML = "Testing";
 elementOne.id = "image";
 document.body.appendChild(elementOne);
+//
+elementOne.innerHTML = "<img src='https://s3.amazonaws.com/learn-verified/painting-with-dog.gif'/>"
+//Image displayed to view
