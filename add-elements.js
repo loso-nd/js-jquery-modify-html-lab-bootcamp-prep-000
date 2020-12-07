bcfa7cc4a1b92d62a91a98db1ae7472bc314fa40
Nahomie ND <nducasse3@gmail.com>
2020-12-07 19:24:10 +0000

    Automatically backed up by Learn

--- add-elements.js
+++ add-elements.js
@@ -1,7 +1,14 @@
 var elementOne = document.createElement('div');
+//Creates and element 
+
 elementOne.innerHTML = "Testing";
-elementOne.id = "image";
+//Creates text inside an exisiting element
+
+elementOne.id = "image"; 
+// Creates an id to an existing element
+
 document.body.appendChild(elementOne);
-//
+//To get an element to appear in the DOM, we append it to an existing DOM node. We can start as high up on the tree as document.body, or we can find a more specific element using any of the techniques we've learned for traversing the DOM.
+
 elementOne.innerHTML = "<img src='https://s3.amazonaws.com/learn-verified/painting-with-dog.gif'/>"
 //Image displayed to view
